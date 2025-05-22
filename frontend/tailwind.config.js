eefcdddfaf0ccfe49273c91a3f30aeb2b1276ca6
PRO-DEVELOPER-1 <ndisaster20@gmail.com>
2025-05-22 21:38:28 +0300

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        neon: {
+          pink: '#ff00ff',
+          blue: '#00ffff',
+          green: '#00ff00',
+          purple: '#9d00ff',
+        },
+      },
+    },
+  },
+  plugins: [],
+}

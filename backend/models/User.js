3970ed8746c783f4c1de0047c18fdf2a77153ae7
PRO-DEVELOPER-1 <ndisaster20@gmail.com>
2025-05-22 20:50:17 +0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,23 +1,9 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  bots: [{ type: mongoose.Schema.Types.ObjectId, ref: "Bot" }],
-});
+  bots: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Bot' }],
+}, { timestamps: true });
 
-export default mongoose.model("User", UserSchema);
-```
-
-#### **`models/Bot.js`**
-```javascript
-import mongoose from "mongoose";
-
-const BotSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  token: { type: String, required: true },
-  status: { type: String, default: "offline" },
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-});
-
-export default mongoose.model("Bot", BotSchema);
+export default mongoose.model('User', UserSchema);

7c1c3dd1735517f051fcde8c4e7bb0930567a488
PRO-DEVELOPER-1 <ndisaster20@gmail.com>
2025-05-22 21:36:52 +0300

    Create Login.jsx

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import LoginForm from '../components/Auth/Login'
+
+export default function Login() {
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <LoginForm />
+    </div>
+  )
+}

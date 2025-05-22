870f9eb1d7925ce4cb26c454507b3594e1b490af
PRO-DEVELOPER-1 <ndisaster20@gmail.com>
2025-05-22 22:23:27 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,7 @@
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:10000', // Matches backend PORT
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+export default {
+  build: {
+    outDir: '../frontend/dist',
+    emptyOutDir: true,
+    sourcemap: false
   }
-})
+}
